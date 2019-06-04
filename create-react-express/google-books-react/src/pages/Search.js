fb834f303c90631ad455a52be6571ddde5e6d046
Cody <cody.tracy92@gmail.com>
2019-06-03 20:24:40 -0500

    added in a search bar and palced it appropriately

--- create-react-express/google-books-react/src/pages/Search.js
+++ create-react-express/google-books-react/src/pages/Search.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import API from "../utils/google-books"
+import { Input, FormBtn } from "../components/search-box";
 
 class Search extends Component{
     state = {
@@ -10,6 +11,13 @@ class Search extends Component{
     render(){
       return (
             <div>
+                <div class="container">
+                    <form>
+                        <Input name="title" placeholder="Title or Author (required)" />
+                        <FormBtn>Search!</FormBtn>
+                    </form>                    
+                </div>
+
                 <p>
                     This Search page is working. 
                 </p>
